82816223a50d0f6209c70dd9cd142034b425343b
Rafał <RafalLopaciuk@github.com>
2018-04-07 09:29:45 +0200

    addProject

---
+++ JsonParser/Models/ParseJson.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace JsonParser.Models
+{
+    class ParseJson
+    {
+        public List<Person> ListOfPersons { get; set; }
+
+        public ParseJson(string filename)
+        {
+            string jsonData = File.ReadAllText(filename);
+
+            ListOfPersons = JsonConvert.DeserializeObject<List<Person>>(jsonData);
+        }
+
+        public int getFemaleCount()
+        {
+            int femaleCount = 0;
+
+            foreach (Person element in ListOfPersons)
+            {
+                if (element.gender == "female") femaleCount++;
+            }
+
+            return femaleCount;
+        }
+
+        public int getMaleCount()
+        {
+            int MaleCount = 0;
+
+            foreach (Person element in ListOfPersons)
+            {
+                if (element.gender == "male") MaleCount++;
+            }
+
+            return MaleCount;
+        }
+
+        public double getAverageAge()
+        {
+            double avg = 0;
+
+            foreach (Person element in ListOfPersons)
+            {
+                avg += element.age;
+            }
+
+            avg /= ListOfPersons.Count;
+
+            return avg;
+        }
+    }
+}
---
+++ JsonParser/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JsonParser.Models;
+
+namespace JsonParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParseJson List = new ParseJson("Data/generatedData.json");
+
+            Console.WriteLine("There are " + List.getFemaleCount() + " women and " + List.getMaleCount() + " men in this data");
+            Console.WriteLine("The average age is: " + List.getAverageAge());
+
+            Console.ReadKey();
+        }
+    }
+}
